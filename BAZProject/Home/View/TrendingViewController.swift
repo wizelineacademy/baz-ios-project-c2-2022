6acceb93c3f6ddf0dc52a21d862840ac9e022036
Alan Emiliano Ramirez Ayala <75821310+Replase@users.noreply.github.com>
2022-10-19 16:26:26 -0500

    Indentacion de código

--- BAZProject/Home/View/TrendingViewController.swift
+++ BAZProject/Home/View/TrendingViewController.swift
@@ -7,14 +7,15 @@
 import UIKit
 
 class TrendingViewController: UITableViewController {
-
+    
     let viewModel = ViewModelMovie()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configurateView()
         refreshData()
-        
     }
+    
     private func configurateView() {
         viewModel.getInfo(.Trendig)
     }
@@ -26,32 +27,32 @@ class TrendingViewController: UITableViewController {
             }
         }
     }
-
+    
 }
 
 // MARK: - TableView's DataSource
 
 extension TrendingViewController {
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         viewModel.dataArray.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         tableView.dequeueReusableCell(withIdentifier: "TrendingTableViewCell")!
     }
-
+    
 }
 
 // MARK: - TableView's Delegate
 
 extension TrendingViewController {
-
+    
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         var config = UIListContentConfiguration.cell()
         config.text = viewModel.dataArray[indexPath.row].title
         config.image = UIImage(data: viewModel.getImage(urlImage: viewModel.dataArray[indexPath.row].poster_path))
         cell.contentConfiguration = config
     }
-
+    
 }
