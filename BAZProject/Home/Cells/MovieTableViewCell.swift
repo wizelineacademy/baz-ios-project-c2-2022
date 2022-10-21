3a331b0aad39ab9d0ab00afff0c8af6d9bc5e33a
Alan Emiliano Ramirez Ayala <75821310+Replase@users.noreply.github.com>
2022-10-21 16:33:48 -0500

    Agregación de animación de carga e indentacion de código

--- BAZProject/Home/Cells/MovieTableViewCell.swift
+++ BAZProject/Home/Cells/MovieTableViewCell.swift
@@ -6,19 +6,31 @@
 //
 
 import UIKit
+import SkeletonView
 
 class MovieTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var infoStackView: UIStackView!
     @IBOutlet weak var imageViewMovie: UIImageView!
     @IBOutlet weak var tituloMovie: UILabel!
     @IBOutlet weak var originalTitleMovie: UILabel!
     @IBOutlet weak var calificacionMovie: UILabel!
+    
     func configure(titulo: String, originalTitle: String, calificacion: Double, image: UIImage) {
         tituloMovie.text = titulo
         originalTitleMovie.text = originalTitle
-        calificacionMovie.text = String(calificacion)
+        calificacionMovie.text = "Calificacion: \(calificacion)"
         imageViewMovie.image = image
     }
-
+    
+    func setUpSkeleton() {
+        imageViewMovie.isSkeletonable = true
+        tituloMovie.linesCornerRadius = 8
+        originalTitleMovie.linesCornerRadius = 8
+        calificacionMovie.linesCornerRadius = 8
+        tituloMovie.isSkeletonable = true
+        originalTitleMovie.isSkeletonable = true
+        calificacionMovie.isSkeletonable = true
+    }
+    
 }
--- BAZProject/Home/View/HomeViewController.swift
+++ BAZProject/Home/View/HomeViewController.swift
@@ -12,13 +12,13 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var tableViewMovies: UITableView! {
         didSet {
-           let nib = UINib(nibName: "MovieCell", bundle: nil)
-           tableViewMovies.register(nib, forCellReuseIdentifier: "MovieCell")
-           tableViewMovies.dataSource = self
-           tableViewMovies.delegate = self
-           tableViewMovies.reloadData()
+            let nib = UINib(nibName: "MovieCell", bundle: nil)
+            tableViewMovies.register(nib, forCellReuseIdentifier: "MovieCell")
+            tableViewMovies.dataSource = self
+            tableViewMovies.delegate = self
+            tableViewMovies.reloadData()
         }
-     }
+    }
     let viewModel = ViewModelMovie()
     
     override func viewDidLoad() {
@@ -54,7 +54,7 @@ class HomeViewController: UIViewController {
     private func configurateView() {
         viewModel.getAllMovies()
     }
-
+    
     private func refreshData() {
         viewModel.refreshData = { [weak self] () in
             DispatchQueue.main.async {
@@ -62,39 +62,32 @@ class HomeViewController: UIViewController {
             }
         }
     }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         self.viewModel.dataArray.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tableViewMovies.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieTableViewCell
-        let imageMovie = UIImage(data: viewModel.getImage(urlImage: viewModel.dataArray[indexPath.row].poster_path)) ?? UIImage(named: "poster")
-        cell.configure(titulo: viewModel.dataArray[indexPath.row].title, originalTitle: viewModel.dataArray[indexPath.row].original_title, calificacion: viewModel.dataArray[indexPath.row].vote_average, image: imageMovie ?? UIImage())
+        cell.setUpSkeleton()
+        cell.tituloMovie.showAnimatedGradientSkeleton()
+        cell.calificacionMovie.showAnimatedGradientSkeleton()
+        cell.imageViewMovie.showAnimatedGradientSkeleton()
+        cell.originalTitleMovie.showAnimatedGradientSkeleton()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [self] in
+            cell.originalTitleMovie.hideSkeleton()
+            cell.tituloMovie.hideSkeleton()
+            cell.calificacionMovie.hideSkeleton()
+            cell.imageViewMovie.hideSkeleton()
+            let imageMovie = UIImage(data: self.viewModel.getImage(urlImage: viewModel.dataArray[indexPath.row].poster_path)) ?? UIImage(named: "poster")
+            cell.configure(titulo: viewModel.dataArray[indexPath.row].title, originalTitle: viewModel.dataArray[indexPath.row].original_title, calificacion: viewModel.dataArray[indexPath.row].vote_average, image: imageMovie ?? UIImage())
+        }
         return cell
     }
     
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
-
-//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-//        var config = UIListContentConfiguration.cell()
-//        config.text = viewModel.dataArray[indexPath.row].title
-//        config.image = UIImage(data: viewModel.getImage(urlImage: viewModel.dataArray[indexPath.row].poster_path))
-//        cell.contentConfiguration = config
-//    }
 }
