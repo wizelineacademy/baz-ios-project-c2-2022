3e841a0fbeb20bf76d7a86871cc3f3290ff8aae4
961082 <961082>
2022-10-24 21:13:35 -0500

    Agregar archivo de extensiones agregue alerta básica para mostrar los errores

---
+++ BAZProject/Extensions/ExtensionUIViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExtensionUIViewController.swift
+//  BAZProject
+//
+//  Created by mvazquezl on 24/10/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func basicAlert(title:String, message:String){
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+}
