19a5c8a23577f31491cc7fc487604822cd65ddfd
1048493 <mayra.carreno@bancoazteca.com>
2022-10-27 10:04:00 -0500

    Se agrega func getImage()

--- BAZProject/MovieAPI.swift
+++ BAZProject/MovieAPI.swift
@@ -10,11 +10,11 @@ class MovieAPI {
     
     private let apiKey: String = "f6cd5c1a9e6c6b965fdcab0fa6ddd38a"
     private let baseURL: String = "https://api.themoviedb.org/3"
+    private let imageURL: String = "https://image.tmdb.org/t/p/w500"
     
-    ///enlaza TrendingViewController con este modelo de la vista
+    ///binding TrendingViewController to this view model
     var refreshData = {()->() in }
     
-    ///Fuente de datos Array
     var dataMovie : ResultsMovie? {
         didSet{
             refreshData()
@@ -32,5 +32,9 @@ class MovieAPI {
                 print("Ha ocurrido un error: \(error.localizedDescription)")
             }
         }.resume()
+    }
+    ///return imageURL
+    func getImage() -> String{
+        return  imageURL
     }
 }
