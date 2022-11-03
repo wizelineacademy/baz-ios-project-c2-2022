c324a465cff0cb2d5193b908cb878d5ee7225123
1048493 <mayra.carreno@bancoazteca.com>
2022-11-02 18:54:14 -0600

    Se implementa ViewController para TrendingViewController, sea agregan vistas a tabbar

--- BAZProject/TrendingViewController.swift
+++ BAZProject/TrendingViewController.swift
@@ -6,8 +6,15 @@
 
 import UIKit
 
-class TrendingViewController: UITableViewController {
-    @IBOutlet var tblMovie: UITableView!
+class TrendingViewController: UIViewController {
+    
+    @IBOutlet var tblMovie: UITableView!{
+        didSet{
+            tblMovie.delegate = self
+            tblMovie.dataSource = self
+            registerNibs()
+        }
+    }
     
     let movieApi = MovieAPI()
     let identifier = "TrendingTableViewCell"
@@ -21,17 +28,21 @@ class TrendingViewController: UITableViewController {
             }
         }
     }
+    
+    private func registerNibs() {
+        tblMovie.register(UINib(nibName: "TrendingTableViewCell", bundle: nil), forCellReuseIdentifier: "TrendingTableViewCell")
+       }
 }
 
 // MARK: - TableView's DataSource
 
-extension TrendingViewController {
+extension TrendingViewController : UITableViewDelegate, UITableViewDataSource {
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movieApi.dataMovie?.results.count ?? 0
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? TrendingTableViewCell else{ return UITableViewCell() }
         let info = movieApi.dataMovie?.results[indexPath.row]
         cell.lblTitle.text = info?.title
