1416bc3714576926f2eb50e0f35e1b420b6d4a1d
victor manzanero loeza <victor.manzanero@globant.com>
2022-11-09 04:11:59 -0600

    commit for new custom button with five styles

---
+++ BAZProject/VisualComponents/CustomButton/CustomButtonStyle.swift
@@ -0,0 +1,16 @@
+//
+//  CustomButtonStyle.swift
+//  BAZProject
+//
+//  Created by victor manzanero on 09/11/22.
+//
+
+import Foundation
+
+enum CustomButtonStyle: Int {
+    case none = 0
+    case primary = 1
+    case secundary = 2
+    case optional = 3
+    case other = 4
+}
