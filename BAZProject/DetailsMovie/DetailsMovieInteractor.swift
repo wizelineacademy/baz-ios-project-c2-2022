87992c07cf7101192ed7dc67bfbac3c1cc1852f8
Fredy Dominguez <fredy.dominguez@elektra.com.mx>
2022-11-11 15:40:32 -0600

    AddCommentsToFunctions

--- BAZProject/DetailsMovie/DetailsMovieInteractor.swift
+++ BAZProject/DetailsMovie/DetailsMovieInteractor.swift
@@ -14,16 +14,23 @@ final class DetailsMovieInteractor: DetailsMovieInteractorInputAndOutputProtocol
     var idMovie: Int = 0
     var presenter: DetailsMoviePresenterProtocols?
     
+    /// setUpPresentToInteractor: config data to present to user
+    ///  - Parameter movie: object type Movie
     func setUpPresentToInteractor(with movie: Movie) {
         let detailMovie = movie.adaptToDetailsMovieModelAdapter(with: arrFavoriteMovies ?? [])
         idMovie = movie.id
         setupInteractorToPresent(with: detailMovie)
     }
     
+    /// setupInteractorToPresent: send object Movie model
+    ///  - Parameter movie: object type DetailsMovieModel
     func setupInteractorToPresent(with movie: DetailsMovieModel) {
         presenter?.setUpPresenterToView(with: movie)
     }
     
+    /// likeButtonTapped: determ icon like into view
+    ///  - Parameter isLiked: bool that determ is like o not the movie
+    ///  - Parameter delegado: DetailMovieDelegate to add or remove into array favorite id´s 
     func likeButtonTapped(isLike: Bool, delegado: DetailsMovieDelegate) {
         if isLike {
             delegado.addMovie(with: idMovie)
--- BAZProject/DetailsMovie/DetailsMovieModelAdapter.swift
+++ BAZProject/DetailsMovie/DetailsMovieModelAdapter.swift
@@ -9,10 +9,16 @@ import UIKit
 
 extension Movie {
     
+    /// adaptToDetailsMovieModelAdapter: mapea Movie model to DetailsMovieModel
+    ///  - Parameter listFavorites: Array movie id´s compare with movie id
+    ///  - Returns: object type DetailsMovieModel
     func adaptToDetailsMovieModelAdapter(with listFavorites: [Int]) -> DetailsMovieModel {
         map(with: self, listFavorites: listFavorites)
     }
     
+    /// map: mapea Movie model to DetailsMovieModel
+    ///  - Parameter movie: object type Movie model
+    ///  - Parameter listFavorites: Array movie id´s compare with movie id
     private func map(with movie: Movie, listFavorites: [Int]) -> DetailsMovieModel {
         let isFavorite = listFavorites.contains(movie.id)
         let favoriteImage = isFavorite ? UIImage(systemName: "heart.fill") : UIImage(systemName: "heart")
