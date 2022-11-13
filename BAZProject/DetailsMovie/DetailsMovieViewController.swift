2749882eed7d4c9a39840c0657c59e468cbd24a6
Fredy Dominguez <fredy.dominguez@elektra.com.mx>
2022-11-13 15:25:05 -0600

    AddSimilarAndRecommendatioMoviesInDetailsMovie

--- BAZProject/DetailsMovie/DetailsMovieViewController.swift
+++ BAZProject/DetailsMovie/DetailsMovieViewController.swift
@@ -20,10 +20,14 @@ final class DetailsMovieViewController: UIViewController {
     @IBOutlet weak var btnLikedMovie: UIButton!
     @IBOutlet weak var recommendationCollection: UICollectionView!
     @IBOutlet weak var similarCollection: UICollectionView!
+    @IBOutlet weak var labelRecommendation: UILabel!
+    @IBOutlet weak var labelSimilar: UILabel!
     
     var presenter: DetailsMoviePresenterProtocols?
     let delegate: DetailsMovieDelegate
     var likedMovie: Bool
+    var similarMovies: [Movie] = []
+    var recommendationMovies: [Movie] = []
     
     init(delegado: DetailsMovieDelegate) {
         self.delegate = delegado
@@ -39,10 +43,19 @@ final class DetailsMovieViewController: UIViewController {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = false
         self.moviesCount()
+        setUpView()
         presenter?.setUpPresentToInteractor()
         NotificationCenter.default.post(name: .countMovieDetails, object: nil)
     }
     
+    private func setUpView(){
+        self.recommendationCollection.dataSource = self
+        self.recommendationCollection.delegate = self
+        self.recommendationCollection.register(UINib(nibName: MoviesCategoryCollectionViewCell.nameCell, bundle: nil), forCellWithReuseIdentifier: MoviesCategoryCollectionViewCell.identifier)
+        self.similarCollection.dataSource = self
+        self.similarCollection.delegate = self
+        self.similarCollection.register(UINib(nibName: MoviesCategoryCollectionViewCell.nameCell, bundle: nil), forCellWithReuseIdentifier: MoviesCategoryCollectionViewCell.identifier)
+    }
     /// setUpImageMovie: config background movie image
     ///  - Parameter moviePath: name background path movie
     private func setUpImageMovie(with moviePath: String) {
@@ -57,7 +70,60 @@ final class DetailsMovieViewController: UIViewController {
             }
         }
     }
+    
+    private func setupSimilarMovies(with idMovie: String) {
+        SimilarAPI().searchSimilarMovies(with: idMovie, completion: { result in
+            switch result {
+            case .success(let res):
+                let similarMovies = res.movies
+                self.similarMovies = similarMovies
+                if similarMovies.isEmpty {
+                    DispatchQueue.main.async {
+                        self.similarCollection.isHidden = true
+                        self.labelSimilar.isHidden = true
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.similarCollection.reloadData()
+                    }
+                }
+            case .failure(let error):
+                debugPrint(error)
+                self.similarMovies = []
+                DispatchQueue.main.async {
+                    self.similarCollection.isHidden = true
+                    self.labelSimilar.isHidden = true
+                }
+            }
+        })
+    }
 
+    private func setupRecommendationMovies(with idMovie: String) {
+        RecommendationAPI().searchRecommendationMovies(with: idMovie, completion: { result in
+            switch result {
+            case .success(let res):
+                let movies = res.movies
+                self.recommendationMovies = movies
+                if movies.isEmpty {
+                    DispatchQueue.main.async {
+                        self.recommendationCollection.isHidden = true
+                        self.labelRecommendation.isHidden = true
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.recommendationCollection.reloadData()
+                    }
+                }
+            case .failure(let error):
+                debugPrint(error)
+                self.recommendationMovies = []
+                DispatchQueue.main.async {
+                    self.recommendationCollection.isHidden = true
+                    self.labelRecommendation.isHidden = true
+                }
+            }
+        })
+    }
     /// clickLikedMovie: Func  execute when user click into like button
     @IBAction func clickLikedMovie() {
         likedMovie = !likedMovie
@@ -73,8 +139,8 @@ extension DetailsMovieViewController: DetailsMovieViewProtocols {
         self.descriptionLbl.text = movie.overview
         self.btnLikedMovie.setImage(UIImage(systemName: isFavorite), for: .normal)
         setUpImageMovie(with: movie.backdropPath ?? "poster")
-        self.recommendationCollection.reloadData()
-        
+        self.setupSimilarMovies(with: "\(movie.id)")
+        self.setupRecommendationMovies(with: "\(movie.id)")
     }
     /// likeIconChange: func change heart icon tapped button like
     ///  - Parameter imagePath: name background path movie
@@ -83,4 +149,40 @@ extension DetailsMovieViewController: DetailsMovieViewProtocols {
             self.btnLikedMovie.setImage(UIImage(systemName: imagePath), for: .normal)
         }
     }
+}
+
+extension DetailsMovieViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == self.recommendationCollection {
+            return self.recommendationMovies.count
+        } else {
+            return self.similarMovies.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == self.recommendationCollection {
+            guard let cell = recommendationCollection.dequeueReusableCell(withReuseIdentifier: MoviesCategoryCollectionViewCell.identifier, for: indexPath) as? MoviesCategoryCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            cell.setUpCell(recommendationMovies[indexPath.row], [])
+            return cell
+        } else if collectionView == self.similarCollection {
+            guard let cell = similarCollection.dequeueReusableCell(withReuseIdentifier: MoviesCategoryCollectionViewCell.identifier, for: indexPath) as? MoviesCategoryCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            cell.setUpCell(similarMovies[indexPath.row], [])
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+}
+
+extension DetailsMovieViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let widthPerItem: CGFloat = 120
+        return CGSize(width: widthPerItem, height: widthPerItem * 1.7)
+    }
+
 }
