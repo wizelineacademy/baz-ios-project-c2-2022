5883a451d8f018c07e639a7fca602f9e437db57f
1048493 <mayra.carreno@bancoazteca.com>
2022-11-13 17:54:47 -0600

    se agrega protocolo  y delegado MovieDataDelegate

--- BAZProject/Main/View/TrendingViewController.swift
+++ BAZProject/Main/View/TrendingViewController.swift
@@ -19,16 +19,14 @@ class TrendingViewController: UIViewController {
     // MARK: - Properties
     let movieApi = MovieAPI()
     let identifier = "TrendingTableViewCell"
+    var arrMovies : ResultsMovie?
+    
     
     //MARK: Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        movieApi.delegado = self
         movieApi.getMovies()
-        movieApi.refreshData = { [weak self]() in
-            DispatchQueue.main.async {
-                self?.tblMovie.reloadData()
-            }
-        }
     }
     
     /// Register the custom cell to Trending
@@ -37,17 +35,27 @@ class TrendingViewController: UIViewController {
     }
 }
 
+//MARK: Delegado Movie
+extension TrendingViewController: MovieDataDelegate{
+    func showMovies(dataMovie: ResultsMovie) {
+        arrMovies = dataMovie
+        DispatchQueue.main.async {
+            self.tblMovie.reloadData()
+        }
+    }
+}
+
 // MARK: - TableView's DataSource
 
 extension TrendingViewController : UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movieApi.dataMovie?.results.count ?? 0
+        return arrMovies?.results.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? TrendingTableViewCell else{ return UITableViewCell() }
-        if let dataMovie = movieApi.dataMovie{
+        if let dataMovie = arrMovies{
             let info = dataMovie.results[indexPath.row]
             cell.getInfoCell(movie: info )
         }
@@ -57,10 +65,12 @@ extension TrendingViewController : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController(nibName: "DetailViewController",
                                       bundle: Bundle(for: DetailViewController.self))
-        if let dataMovie = movieApi.dataMovie{
+        if let dataMovie = arrMovies{
             vc.dataMovie = dataMovie.results[indexPath.row]
             self.navigationController?.pushViewController(vc, animated: true)
         }
     }
 }
+
+
 
--- BAZProject/Main/ViewModel/MovieAPI.swift
+++ BAZProject/Main/ViewModel/MovieAPI.swift
@@ -7,20 +7,16 @@
 import Foundation
 import UIKit
 
+protocol MovieDataDelegate{
+    func showMovies(dataMovie: ResultsMovie)
+}
+
 class MovieAPI {
-    
+    var delegado: MovieDataDelegate?
     private let apiKey: String = "f6cd5c1a9e6c6b965fdcab0fa6ddd38a"
     private let baseURL: String = "https://api.themoviedb.org/3"
     private let imageURL: String = "https://image.tmdb.org/t/p/w500"
     
-    ///binding TrendingViewController to this view model
-    var refreshData = {()->() in }
-    
-    var dataMovie : ResultsMovie? {
-        didSet{
-            refreshData()
-        }
-    }
     
     func getMovies() {
         guard let url = URL(string: "\(baseURL)/trending/movie/day?api_key=\(apiKey)") else {return}
@@ -28,7 +24,9 @@ class MovieAPI {
             guard let json = data else{return}
             do{
                 let decoder = JSONDecoder()
-                self.dataMovie = try decoder.decode(ResultsMovie.self, from: json)
+                let dataMovie = try decoder.decode(ResultsMovie.self, from: json)
+                delegado?.showMovies(dataMovie: dataMovie)
+                
             }catch let error{
                 print("Ha ocurrido un error: \(error.localizedDescription)")
             }
